0f3e73e5f884c274e3e6f94a66091f46dbbf8583
InshadS <inshad@hamon.in>
2022-07-27 10:46:26 +0530

    Initialized home page design

---
+++ src/components/MainContainer/MainContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MainContainer.css';
+
+const MainContainer = () => {
+  return (
+    <div className='container'>
+      Time Tracker
+      <div>
+        <button className='btn'>Add</button>
+      </div>
+    </div>
+  );
+};
+
+export default MainContainer;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className='navbar d-flex align-items-center'>
+      <div> Time Tracker</div>
+    </div>
+  );
+}
+
+export default Navbar;
