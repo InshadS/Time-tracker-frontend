29c64438bcbf8f37691ab787a9257b248bc9e3ce
InshadS <inshad@hamon.in>
2022-07-28 16:02:46 +0530

    Added text input for task

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,11 @@ import './style.css';
 function Home() {
   return (
     <div className='home'>
-      <input className='track-input' type='text' />
+      <div className='input-container'>
+        <input type='text' placeholder='What are you working on?' />
+        <span>00:00:00</span>
+        <button>Start</button>
+      </div>
     </div>
   );
 }
